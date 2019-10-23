6a77e5d5e72e2e60a6526a9ad0b05de5902b6a53
manulangat <emmanuelthedeveloper@gmail.com>
2019-10-23 17:16:43 +0300

    add the c

--- dioflames/settings.py
+++ dioflames/settings.py
@@ -137,7 +137,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
-STATICFILES_DIRS = []
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'frontend/static'),
+]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
